d8c161c8d73e604b3a0c2ad4542d8641bc37a63f
Aurimas Žarskis <a.zarskis@paysera.lt>
2019-12-10 13:23:34 +0200

    Minor changes

--- src/main/kotlin/com/paysera/lib/blacklist/entities/RestrictionFIlter.kt
+++ src/main/kotlin/com/paysera/lib/blacklist/entities/RestrictionFIlter.kt
@@ -1,7 +1,7 @@
 package com.paysera.lib.blacklist.entities
 
 data class RestrictionFIlter(
-    val statuses: List<String>,
-    val hidden: Boolean,
-    val userId: Int?
+    val statuses: List<String>?,
+    val hidden: Boolean?,
+    val userId: Int
 )
