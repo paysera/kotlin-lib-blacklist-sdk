9c37f2717ba2f20b535713707e61c5a47c941b17
Edgar Žigis <tranced.freak@gmail.com>
2020-08-20 07:45:25 +0300

    Implement logging interface

--- src/main/kotlin/com/paysera/lib/blacklist/retrofit/NetworkApiFactory.kt
+++ src/main/kotlin/com/paysera/lib/blacklist/retrofit/NetworkApiFactory.kt
@@ -2,6 +2,7 @@ package com.paysera.lib.blacklist.retrofit
 
 import com.paysera.lib.blacklist.clients.BlacklistApiClient
 import com.paysera.lib.common.entities.ApiCredentials
+import com.paysera.lib.common.interfaces.ErrorLoggerInterface
 import com.paysera.lib.common.interfaces.TokenRefresherInterface
 import com.paysera.lib.common.retrofit.BaseApiFactory
 import okhttp3.logging.HttpLoggingInterceptor
@@ -10,12 +11,14 @@ class NetworkApiFactory(
     userAgent: String?,
     credentials: ApiCredentials,
     timeout: Long? = null,
-    httpLoggingInterceptorLevel: HttpLoggingInterceptor.Level = HttpLoggingInterceptor.Level.BASIC
+    httpLoggingInterceptorLevel: HttpLoggingInterceptor.Level = HttpLoggingInterceptor.Level.BASIC,
+    errorLoggerInterface: ErrorLoggerInterface
 ) : BaseApiFactory<BlacklistApiClient>(
     userAgent,
     credentials,
     timeout,
-    httpLoggingInterceptorLevel
+    httpLoggingInterceptorLevel,
+    errorLoggerInterface
 ) {
     override val baseUrl = "https://blacklist.paysera.com/restriction/rest/v1/"
     override val certifiedHosts = listOf("blacklist.paysera.com")
