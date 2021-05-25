23ff5ea0b1e3313111492cd2db5522a9f7a9a7d8
Gilbert Parreno <gilbertparreno95@gmail.com>
2021-05-25 13:44:52 +0800

    migrate to kotlin-dsl and set up for maven central

---
+++ buildSrc/src/main/kotlin/ApplicationConfiguration.kt
@@ -0,0 +1,10 @@
+object ApplicationConfiguration {
+    const val artifactId = "kotlin-lib-blacklist-sdk"
+    const val groupId = "com.paysera.lib"
+    const val version = "1.9.1-SNAPSHOT"
+    const val name = "Kotlin Library Blacklist SDK"
+    const val url = "https://www.paysera.com/v2/en-GB/index"
+    const val description = "Paysera: Kotlin Blacklist SDK Library"
+    const val releaseRepositoryUrl = "https://s01.oss.sonatype.org/content/repositories/releases/"
+    const val snapshotRepositoryUrl = "https://s01.oss.sonatype.org/content/repositories/snapshots/"
+}
