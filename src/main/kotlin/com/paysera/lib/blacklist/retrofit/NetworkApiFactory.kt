0287686c3488003da4ddcf932e454f412b05924c
Gilbert Parreno <gilbertparreno95@gmail.com>
2022-04-11 10:49:58 +0800

    APP-3420: New users can not open accounts

--- src/main/kotlin/com/paysera/lib/blacklist/retrofit/NetworkApiFactory.kt
+++ src/main/kotlin/com/paysera/lib/blacklist/retrofit/NetworkApiFactory.kt
@@ -1,7 +1,7 @@
 package com.paysera.lib.blacklist.retrofit
 
 import com.paysera.lib.blacklist.clients.BlacklistApiClient
-import com.paysera.lib.common.interfaces.BaseApiCredentials
+import com.paysera.lib.common.entities.AuthorizationApiCredentials
 import com.paysera.lib.common.interfaces.ErrorLoggerInterface
 import com.paysera.lib.common.interfaces.TokenRefresherInterface
 import com.paysera.lib.common.retrofit.BaseApiFactory
@@ -11,7 +11,7 @@ class NetworkApiFactory(
     baseUrl: String,
     locale: String?,
     userAgent: String?,
-    credentials: BaseApiCredentials,
+    credentials: AuthorizationApiCredentials,
     certifiedHosts: List<String> = emptyList(),
     timeout: Long? = null,
     httpLoggingInterceptorLevel: HttpLoggingInterceptor.Level = HttpLoggingInterceptor.Level.BASIC,
