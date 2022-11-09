821dbe8fdfb4925bc4f9e427c92b50733c9ff00e
GIlbert Parreño <gilbertparreno95@gmail.com>
2022-11-09 11:48:45 +0800

    APP-3649: Implement InRento API (#17)
    * APP-3649: Implement InRento API
    
    * refactor api credentials
    
    * remove snapshot

--- src/main/kotlin/com/paysera/lib/blacklist/retrofit/NetworkApiFactory.kt
+++ src/main/kotlin/com/paysera/lib/blacklist/retrofit/NetworkApiFactory.kt
@@ -1,7 +1,7 @@
 package com.paysera.lib.blacklist.retrofit
 
 import com.paysera.lib.blacklist.clients.BlacklistApiClient
-import com.paysera.lib.common.entities.AuthorizationApiCredentials
+import com.paysera.lib.common.interfaces.BaseApiCredentials
 import com.paysera.lib.common.interfaces.ErrorLoggerInterface
 import com.paysera.lib.common.interfaces.TokenRefresherInterface
 import com.paysera.lib.common.retrofit.BaseApiFactory
@@ -12,7 +12,7 @@ class NetworkApiFactory(
     baseUrl: String,
     locale: String?,
     userAgent: String?,
-    credentials: AuthorizationApiCredentials,
+    credentials: BaseApiCredentials,
     timeout: Long? = null,
     httpLoggingInterceptorLevel: HttpLoggingInterceptor.Level = HttpLoggingInterceptor.Level.BASIC,
     errorLogger: ErrorLoggerInterface,
